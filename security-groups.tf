fa4afa7d55a7b16f5249d0e5a12dd8845d421a42
Oluwagbenga Paul Idowu <paulig_id@yahoo.com>
2025-04-09 04:23:06 -0600

    Fixed aws_security_group and all_worker_mgmt in security-groups.tf

--- security-groups.tf
+++ security-groups.tf
@@ -5,28 +5,32 @@
 # and outbound internet access.
 # -----------------------------------------------
 
-module "all_worker_mgmt_sg" {
-  source  = "terraform-aws-modules/security-group/aws"
-  version = "5.3.0"
 
-  name        = "all-worker-mgmt"
-  description = "Security group to allow EKS worker nodes communication and internet access"
+resource "aws_security_group" "all_worker_mgmt" {
+  name_prefix = "all_worker_management"
   vpc_id      = module.vpc.vpc_id
+}
 
-  # Allow all traffic internally within private CIDRs
-  ingress_rules        = ["all-all"]
-  ingress_cidr_blocks  = [
+resource "aws_security_group_rule" "all_worker_mgmt_ingress" {
+  description       = "allow inbound traffic from eks"
+  from_port         = 0
+  protocol          = "-1"
+  to_port           = 0
+  security_group_id = aws_security_group.all_worker_mgmt.id
+  type              = "ingress"
+  cidr_blocks = [
     "10.0.0.0/8",
     "172.16.0.0/12",
-    "192.168.0.0/16"
+    "192.168.0.0/16",
   ]
+}
 
-  # Allow all outbound traffic
-  egress_rules         = ["all-all"]
-
-  tags = {
-    Name    = "all-worker-mgmt"
-    Owner   = "Pauligtech"  # Optional: update with your name for easier identification
-    Purpose = "EKS Worker Node Management"
-  }
+resource "aws_security_group_rule" "all_worker_mgmt_egress" {
+  description       = "allow outbound traffic to anywhere"
+  from_port         = 0
+  protocol          = "-1"
+  security_group_id = aws_security_group.all_worker_mgmt.id
+  to_port           = 0
+  type              = "egress"
+  cidr_blocks       = ["0.0.0.0/0"]
 }
